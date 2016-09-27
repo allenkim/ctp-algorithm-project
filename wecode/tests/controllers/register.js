007a52d3b57a27dd4e5093baa8aa2578bc30d76e
Allen Kim <allenkim95@gmail.com>
2016-09-27 12:43:04 -0400

    Add sample test files

---
+++ wecode/tests/controllers/register.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../../app');
+var expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Register Controller', () => {
+  it('should display register button');
+});
