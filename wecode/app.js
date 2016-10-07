aded3749cd0fc713f09aad37f66cf7e8c4758d77
KhadeejaDin <khadeejadin@gmail.com>
2016-10-07 10:06:18 -0400

    Add index.js to controllers

--- wecode/app.js
+++ wecode/app.js
@@ -1,25 +1,8 @@
 var express = require('express');
 var app = express();
 
+
 // loading and mounting controllers
-// only requests to /* will be sent to our "problem" router
-const problem = require('./controllers/problem');
-app.use('/', problem);
-
-// only requests to /user/* will be sent to our "profile" router
-const profile = require('./controllers/profile');
-app.use('/user', profile);
-
-// only requests to /login/* will be sent to our "login" router
-const login = require('./controllers/login');
-app.use('/login', login);
-
-// only requests to /register/* will be sent to our "register" router
-const register = require('./controllers/register');
-app.use('/register', register);
-
-// only requests to /results/* will be sent to our "results" router
-const results = require('./controllers/results');
-app.use('/results', results);
+app.use(require('./controllers/'));
 
 app.listen(8000);
