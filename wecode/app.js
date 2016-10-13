ba2f5bd992d40a193a356597392dcbfb843a6982
KhadeejaDin <khadeejadin@gmail.com>
2016-10-12 20:38:35 -0400

    Remove routes from aap.js

--- wecode/app.js
+++ wecode/app.js
@@ -15,30 +15,4 @@ app.use(passport.session());
 // loading and mounting controllers
 app.use(require('./controllers/'));
 
-/*
-// only requests to /* will be sent to our "home" router
-const home = require('./controllers/home');
-app.use('/', home);
-
-// only requests to /problem/* will be sent to our "problem" router
-const problem = require('./controllers/problem');
-app.use('/problem', problem);
-
-// only requests to /user/* will be sent to our "profile" router
-const profile = require('./controllers/profile');
-app.use('/username', profile);
-
-// only requests to /login/* will be sent to our "login" router
-const login = require('./controllers/login');
-app.use('/login', login);
-
-// only requests to /register/* will be sent to our "register" router
-const register = require('./controllers/register');
-app.use('/register', register);
-
-// only requests to /results/* will be sent to our "results" router
-const results = require('./controllers/results');
-app.use('/results', results);
-*/
-
 app.listen(8000);
