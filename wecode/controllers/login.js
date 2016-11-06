7c8ad7869a552b60c1317daa727d98b10c12b5af
KhadeejaDin <khadeejadin@gmail.com>
2016-11-06 17:18:09 -0500

    Login and Authentication works

--- wecode/controllers/login.js
+++ wecode/controllers/login.js
@@ -10,28 +10,22 @@ module.exports = {
     var router = express.Router();
 
     //Respond to GET request on the login route (/login)
-    router.get('/', redirect.ifLoggedIn('/user'), this.index);
+    router.get('/', redirect.ifLoggedIn('/profile'), this.index);
 
     //Respond to POST request on the login route (/login)
-    //router.post('/', this.login);
-
-    router.post('/',
-    passport.authenticate('login', { failureRedirect: '/login' }),
-    function(req, res) {
-    res.redirect('/');
-  });
+    router.post('/', this.login);
 
     return router;
   },
   index(req, res) {
     res.render('login', {error: req.flash('error')});
   },
-  login(req, res) {
+  login(req, res, next) {
     passport.authenticate('login', {
-      successRedirect: '/',
-      failureRedirect: '/problem',
+      successRedirect: '/profile',
+      failureRedirect: '/login',
       failureFlash: true,
       successFlash: true,
-    })(req, res);
+    })(req, res, next);
   },
 };
--- wecode/middlewares/authentication.js
+++ wecode/middlewares/authentication.js
@@ -2,45 +2,22 @@
 var bcrypt = require('bcrypt-nodejs');
 // Load passport module, an authentication middleware for Node.js
 // Strategy to authenticate user: LocalStrategy
-var passport = require('passport')
-  , LocalStrategy = require('passport-local').Strategy;
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var User = require('../models').user;
 
 // verifying incoming password against the hashed password
-function passwordsMatch(passwordSubmitted, passwordStored) {
-  return bcrypt.compare(passwordSubmitted, passwordStored, function(err, res) {
+function passwordsMatch(passwordSubmitted, passwordStored, cb) {
+  bcrypt.compare(passwordSubmitted, passwordStored, function(err, res){
     if (err) {
-      return console.error(err)
+      console.log(err);
     }
+    cb(null, res);
   });
 }
 
-/*
-function passwordsMatch(passwordSubmitted, storedPassword) {
-  return bcrypt.compareSync(passwordSubmitted, storedPassword);
-}
-*/
-
-/*
-// configuring LocalStrategy
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({username: username}, function(user) {
-      if (user) {
-        if (passwordsMatch(password, user.password) === false) {
-          return done(null, false, { message: 'Incorrect password.'});
-        }
-      }
-      else if (user == null) {
-        return done(null, false, { message: 'Incorrect username.'});
-      }
-      return done(null, user, { message: 'Successfully logged In.'});
-    });
-  }
-));
-*/
-
+//Registration strategy
 passport.use(new LocalStrategy({
   usernameField: 'username',
 },
@@ -61,44 +38,46 @@ passport.use(new LocalStrategy({
   })
 );
 
-
+//login authentication strategy
 passport.use('login', new LocalStrategy({
-    passReqToCallback : true
-  },  {
   usernameField: 'username',
-},
-  (username, password, done) => {
-    User.findOne({
-      where: { username },
-    }).then((user) => {
-      if (user) {
-        if (passwordsMatch(password, user.password) === false) {
-          return done(null, false, { message: 'Incorrect password.' });
+  passwordField: 'password',
+  passReqToCallback : true
+ },
+ function(req, username, password, done) {
+     User.findOne({
+       where: { username },
+     }).then((user) => {
+        //check if user exists
+        if (!user) {
+          return done(null, false, { message: 'Incorrect email.' });
         }
-      } else if (user == null) {
-        return done(null, false, { message: 'Incorrect email.' });
-      }
-
-      return done(null, user, { message: 'Successfully Logged In!' });
-    });
-  })
-);
+        //check if password matches
+        passwordsMatch(password, user.password, function(err, res){
+           if (res === false){
+             return done(null, false, { message: 'Incorrect password.' });
+           }
+           else {
+             return done(null, user, { message: 'Successfully Logged In!' });
+          }});
+     });
+}));
 
 
 // serialize user instance to session with only the username
 passport.serializeUser(function(user, done) {
-  done(null, user.username);
+  done(null, user.user_id);
 })
 
 // deserialize user instance from session
-passport.deserializeUser(function(username, done) {
-  User.findById(username, function(user) {
-    if (user == null) {
-      return done(null, false);
+passport.deserializeUser(function(id, done) {
+  User.findById(id).then(function(user) {
+    done(null, user);
+  }).catch(function(err) {
+    if (err) {
+      throw err;
     }
-
-    return done(null, user);
-  });
+ });
 });
 
 passport.redirectIfLoggedIn = (route) =>
