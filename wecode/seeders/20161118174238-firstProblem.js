0e20709feb919ac432c41da316da32e70522bec6
Alan Chen <alanc1995@gmail.com>
2016-11-18 14:18:20 -0500

    Question seeds

--- wecode/seeders/20161118174238-firstProblem.js
+++ wecode/seeders/20161118174238-firstProblem.js
@@ -1,26 +1,54 @@
 'use strict';
 
+var input = `14
+-927101048 -927101048
+-618970250 648940120
+-804722077 -964548842
+-366618439 -188307001
+380565241 165732307
+918809675 -215309091
+842843672 -953850991
+363785686 728361547
+31682488 31682488
+288101856 868511451
+-325935686 -595215593
+-382147959 231270880
+-2 -2
+-879489962 -88890426
+`;
+
+var output = `=
+<
+<
+>
+>
+>
+>
+<
+=
+>
+<
+>
+=
+<
+`;
+
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
+      return queryInterface.bulkInsert('questions', [{
+        question_title: 'Relational Operators',
+        input: input,
+        output: output,
+        question_content: 'https://uva.onlinejudge.org/external/111/11172.pdf',
+        number_attempts: 0,
+        number_success: 0,
+        date: '2016/10/12',
+        createdAt: '2016/10/12',
+        updatedAt: '2016/10/12'
       }], {});
-    */
   },
 
   down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
+    return queryInterface.bulkDelete('questions', null, {});
   }
 };
